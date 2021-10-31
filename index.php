1e9294e6dc20b0b5a17638110337e39898e0e544
LilPub <hiep140701@gmail.com>
2021-10-31 23:20:46 +0700

    update index

--- index.php
+++ index.php
@@ -304,7 +304,6 @@
     
 
 
- _________________________________________________________ -->
     <script src="js/jquery-1.11.0.min.js"></script>
     <script src="js/bootstrap.min.js"></script>
     <script src="js/jquery.cookie.js"></script>
