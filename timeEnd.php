cda18da54b837cdb77ca3acf5faa793ed8229b80
LilPub <hiep140701@gmail.com>
2021-11-04 17:25:33 +0700

    update timeEnd

--- timeEnd.php
+++ timeEnd.php
@@ -1,5 +1,6 @@
 
 <?php
+    session_start();
     $db = mysqli_connect('localhost','root','','shop')
     or die('Kết nối thất bại.'); 
     $ItemNo = $_GET['ItemNo'];
@@ -12,5 +13,14 @@
     }else{
         echo 'Thất bại';
     }
+    $sql = "SELECT * FROM item WHERE ItemID='$ItemNo'";
+    $result = mysqli_query($db, $sql);
+    $info = mysqli_fetch_array($result);
+    $sqlPrice = "UPDATE item SET CurrentPrice=".$info['ExpectedPrice']." WHERE ItemID='$ItemNo'";
+    mysqli_query($db, $sqlPrice);
+    $userID=$_SESSION['userid'];
+    $value = $info['ExpectedPrice'];
+    $sql2="INSERT INTO bids (ItemID,BidderID,BidAmount)VALUES ('$ItemNo','$userID','$value') " ;
+    mysqli_query($db, $sql2);
     header("Location:detail.php?ItemNo=$ItemNo");
 ?>
