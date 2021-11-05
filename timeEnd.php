a688f8180f4451e9cd4556d1d93af5acee4249cd
LilPub <hiep140701@gmail.com>
2021-11-05 21:42:44 +0700

    update time End

--- timeEnd.php
+++ timeEnd.php
@@ -13,14 +13,20 @@
     }else{
         echo 'Thất bại';
     }
+
     $sql = "SELECT * FROM item WHERE ItemID='$ItemNo'";
     $result = mysqli_query($db, $sql);
     $info = mysqli_fetch_array($result);
     $sqlPrice = "UPDATE item SET CurrentPrice=".$info['ExpectedPrice']." WHERE ItemID='$ItemNo'";
     mysqli_query($db, $sqlPrice);
+
     $userID=$_SESSION['userid'];
     $value = $info['ExpectedPrice'];
     $sql2="INSERT INTO bids (ItemID,BidderID,BidAmount)VALUES ('$ItemNo','$userID','$value') " ;
     mysqli_query($db, $sql2);
+
+    $Price = $info['ExpectedPrice'];
+    $sql3 = "INSERT INTO solditems(ItemID,BuyerID,Date,FinalValue) VALUES ('$ItemNo', '$userID', '$timeEnd', '$Price')";
+    mysqli_query($db, $sql3);
     header("Location:detail.php?ItemNo=$ItemNo");
 ?>
